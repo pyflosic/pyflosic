a7b7eddafcb492ed0b1a134f5b447fdc1bd47e4e
Sebastian Schwalbe <48414071+theonov13@users.noreply.github.com>
2019-04-12 13:10:26 +0200

    Add files via upload

---
+++ examples/ase_pyflosic_calculator/03_flosic_scf.py
@@ -0,0 +1,11 @@
+from ase.io import read 
+from ase_pyflosic_calculator import * 
+
+# Read structure (Nuclei + FODs) as xyz file. 
+atoms = read('LiH.xyz')
+# Create pyflosic_ase_calculator object. 
+calc = PYFLOSIC(mode='flosic-scf',atoms=atoms,charge=0,spin=0,xc='LDA,PW',basis='STO3G')
+print(calc.get_energy())
+print(calc.get_forces())
+print(calc.get_dipole_moment())
+print(calc.get_evalues())
