8e62a4114c5b206e14a3adf43634f890bd1442fa
Sebastian Schwalbe <48414071+theonov13@users.noreply.github.com>
2019-04-12 13:04:54 +0200

    Add files via upload

---
+++ utils/test/nrl2py.sh
@@ -0,0 +1,5 @@
+module load  modenv/scs5 pyscf/1.6.0-intel-2018a-Python-3.6.4
+export PYTHONPATH=/home/sschwalb/__Programms__/ase/:$PYTHONPATH
+#export PYTHONPATH=/home/jakr872a/src/ase/:$PYTHONPATH
+export LD_PRELOAD=$MKLROOT/lib/intel64/libmkl_avx2.so:$MKLROOT/lib/intel64/libmkl_sequential.so:$MKLROOT/lib/intel64/libmkl_core.so
+python3 ../nrl2py.py
