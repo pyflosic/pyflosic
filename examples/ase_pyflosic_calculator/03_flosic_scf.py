044b592252a3c37f85ec6df2fd2d9ef3ca04ba19
Jakob Kraus <kraus@theo.physik.tu-freiberg.de>
2020-06-15 13:43:23 +0200

    update

--- examples/ase_pyflosic_calculator/03_flosic_scf.py
+++ examples/ase_pyflosic_calculator/03_flosic_scf.py
@@ -5,7 +5,7 @@ from ase_pyflosic_calculator import *
 atoms = read('LiH.xyz')
 # Create pyflosic_ase_calculator object. 
 calc = PYFLOSIC(mode='flosic-scf',atoms=atoms,charge=0,spin=0,xc='LDA,PW',basis='STO3G')
-print(calc.get_energy())
+print(calc.get_potential_energy())
 print(calc.get_forces())
 print(calc.get_dipole_moment())
 print(calc.get_evalues())
