78c8462ed97bfa050606f7b1d4217248675fff90
Jakob Kraus <kraus@theo.physik.tu-freiberg.de>
2020-06-15 13:53:59 +0200

    update

--- src/ase_pyflosic_optimizer.py
+++ src/ase_pyflosic_optimizer.py
@@ -28,11 +28,9 @@ from ase.optimize.sciopt import SciPyFminCG
 from ase_pyflosic_calculator import PYFLOSIC
 from ase.constraints import FixAtoms 
 from pyscf.data import radii
+from ase.units import Ha,Bohr
 
-def flosic_optimize(mode,atoms,charge,spin,xc,basis,ecp,opt='FIRE',maxstep=0.2,label='OPT_FRMORB',fmax=0.0001,steps=1000,max_cycle=300,conv_tol=1e-5,grid=7,ghost=False,use_newton=False,use_chk=False,verbose=0,debug=False,efield=None,l_ij=None,ods=None,fopt='force',fix_fods=False,ham_sic='HOO',vsic_every=1,dm=None,cart=False,output=None,solvation=None,lmax=10,eta=0.1,lebedev_order=89,radii_table=radii.VDW,eps=78.3553):
-    # -----------------------------------------------------------------------------------
-    # Input 
-    # -----------------------------------------------------------------------------------
+def flosic_optimize(mode='flosic-scf',atoms,charge=0,spin=0,xc='lda,pw',basis='STO-3G',ecp=None,opt='FIRE',maxstep=0.2,label='OPT_FRMORB',fmax=1.5e-4*(Ha/Bohr),steps=1000,max_cycle=300,conv_tol=1e-6,grid=3,newton=False,use_chk=False,verbose=4,efield=None,ham_sic='HOOOV',dm=None,cart=False,output=None,solvation=None,lmax=10,eta=0.1,lebedev_order=89,radii_table=None,eps=78.3553):
     # mode 			dft only optimizes nuclei positions 
     #				flosic only optimizes FOD positions (one-shot)
     #				flosic-scf only optimizes FOD positions (self-consistent)
@@ -43,9 +41,6 @@ def flosic_optimize(mode,atoms,charge,spin,xc,basis,ecp,opt='FIRE',maxstep=0.2,l
     # basis	 		GTO basis set 
     # ecp			if an ECP basis set is used, you must give this extra argument 
     # opt 			optimizer (FIRE, LBFGS, ...) 
-    # ----------------------------------------------------------------------------------
-    # Additional/optional input 
-    # ----------------------------------------------------------------------------------
     # maxstep			stepwidth of the optimizer  
     # label			label for the outputs (logfile and trajectory file) 
     # fmax 			maximum absolute force on each atom
@@ -53,16 +48,10 @@ def flosic_optimize(mode,atoms,charge,spin,xc,basis,ecp,opt='FIRE',maxstep=0.2,l
     # max_cycle     	        maximum number of SCF cycles 
     # conv_tol			energy threshold 
     # grid			numerical mesh 
-    # ghost 			use ghost atom at positions of FODs 
-    # use_newton		use newton SCF cycle 
+    # newton		        use newton SCF cycle 
     # use_chk			restart from chk file 
     # verbose 			output verbosity 
-    # debug			extra output for debugging reasons 
     # efield 			applying an external efield 
-    # l_ij			developer option: another optimitzation criterion, do not use for production 
-    # ods			developer option orbital damping sic, rescale SIC, do not use for production 
-    # fopt			optimization target, default: FOD forces 
-    # fix_fods			freeze FODS during the optimization, might use for 1s/2s FODs 
     # ham_sic			the different unified Hamiltonians HOO and HOOOV 
     # dm                        density matrix
     # cart                      use Cartesian GTO basis and integrals (6d,10f,15g etc.)
@@ -79,12 +68,9 @@ def flosic_optimize(mode,atoms,charge,spin,xc,basis,ecp,opt='FIRE',maxstep=0.2,l
     mode = mode.lower()
     
     
-    if fix_fods != False:
-        c = FixAtoms(fix_fods)
-        atoms.set_constraint(c)
 	
     # set up the ase calculator
-    calc = PYFLOSIC(atoms=atoms,charge=charge,spin=spin,xc=xc,basis=basis,mode=mode,ecp=ecp,max_cycle=max_cycle,conv_tol=conv_tol,grid=grid,ghost=ghost,use_newton=use_newton,verbose=verbose,debug=debug,efield=efield,l_ij=l_ij,ods=ods,fopt=fopt,ham_sic=ham_sic,vsic_every=vsic_every,use_chk=use_chk,dm=dm,cart=cart,output=output,solvation=solvation,lmax=lmax,eta=eta,lebedev_order=lebedev_order,radii_table=radii_table,eps=eps)
+    calc = PYFLOSIC(atoms=atoms,charge=charge,spin=spin,xc=xc,basis=basis,mode=mode,ecp=ecp,max_cycle=max_cycle,conv_tol=conv_tol,grid=grid,newton=newton,verbose=verbose,efield=efield,ham_sic=ham_sic,use_chk=use_chk,dm=dm,cart=cart,output=output,solvation=solvation,lmax=lmax,eta=eta,lebedev_order=lebedev_order,radii_table=radii_table,eps=eps)
     
     # assign the ase calculator to the ase atoms object
     atoms.set_calculator(calc)
