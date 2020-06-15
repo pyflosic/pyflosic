89050efe365dadc8d86d9424676b7df5fbf60a23
Jakob Kraus <kraus@theo.physik.tu-freiberg.de>
2020-06-15 17:45:38 +0200

    update

--- src/ase_pyflosic_calculator.py
+++ src/ase_pyflosic_calculator.py
@@ -305,6 +305,8 @@ class PYFLOSIC(Calculator):
                 mf = mf.newton()
             if self.efield is not None:
                 self.apply_electric_field(mf, self.efield)
+            if self.df:
+                mf = mf.density_fit()
             self.mf = mf
             if self.dm is None:
                 self.mf.kernel()
