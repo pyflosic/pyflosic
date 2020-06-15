8f36738ebcba613f077d20e1acc8474396019fed
Jakob Kraus <kraus@theo.physik.tu-freiberg.de>
2020-06-15 14:02:45 +0200

    update

--- src/ase_pyflosic_optimizer.py
+++ src/ase_pyflosic_optimizer.py
@@ -30,7 +30,7 @@ from ase.constraints import FixAtoms
 from pyscf.data import radii
 from ase.units import Ha,Bohr
 
-def flosic_optimize(mode='flosic-scf',atoms,charge=0,spin=0,xc='lda,pw',basis='STO-3G',ecp=None,opt='FIRE',maxstep=0.2,label='OPT_FRMORB',fmax=1.5e-4*(Ha/Bohr),steps=1000,max_cycle=300,conv_tol=1e-6,grid=3,newton=False,use_chk=False,verbose=4,efield=None,ham_sic='HOOOV',dm=None,cart=False,output=None,solvation=None,lmax=10,eta=0.1,lebedev_order=89,radii_table=None,eps=78.3553):
+def flosic_optimize(mode='flosic-scf',atoms,charge=0,spin=0,xc='lda,pw',basis='STO-3G',ecp=None,opt='FIRE',maxstep=0.2,label='OPT_FRMORB',fmax=1.5e-4*(Ha/Bohr),steps=1000,max_cycle=300,conv_tol=1e-6,grid=3,df=True,newton=False,use_chk=False,verbose=4,efield=None,ham_sic='HOOOV',dm=None,cart=False,output=None,solvation=None,lmax=10,eta=0.1,lebedev_order=89,radii_table=None,eps=78.3553):
     # mode 			dft only optimizes nuclei positions 
     #				flosic only optimizes FOD positions (one-shot)
     #				flosic-scf only optimizes FOD positions (self-consistent)
@@ -48,6 +48,7 @@ def flosic_optimize(mode='flosic-scf',atoms,charge=0,spin=0,xc='lda,pw',basis='S
     # max_cycle     	        maximum number of SCF cycles 
     # conv_tol			energy threshold 
     # grid			numerical mesh 
+    # df                        density fitting
     # newton		        use newton SCF cycle 
     # use_chk			restart from chk file 
     # verbose 			output verbosity 
@@ -70,7 +71,7 @@ def flosic_optimize(mode='flosic-scf',atoms,charge=0,spin=0,xc='lda,pw',basis='S
     
 	
     # set up the ase calculator
-    calc = PYFLOSIC(atoms=atoms,charge=charge,spin=spin,xc=xc,basis=basis,mode=mode,ecp=ecp,max_cycle=max_cycle,conv_tol=conv_tol,grid=grid,newton=newton,verbose=verbose,efield=efield,ham_sic=ham_sic,use_chk=use_chk,dm=dm,cart=cart,output=output,solvation=solvation,lmax=lmax,eta=eta,lebedev_order=lebedev_order,radii_table=radii_table,eps=eps)
+    calc = PYFLOSIC(atoms=atoms,charge=charge,spin=spin,xc=xc,basis=basis,mode=mode,ecp=ecp,max_cycle=max_cycle,conv_tol=conv_tol,grid=grid,df=df,newton=newton,verbose=verbose,efield=efield,ham_sic=ham_sic,use_chk=use_chk,dm=dm,cart=cart,output=output,solvation=solvation,lmax=lmax,eta=eta,lebedev_order=lebedev_order,radii_table=radii_table,eps=eps)
     
     # assign the ase calculator to the ase atoms object
     atoms.set_calculator(calc)
