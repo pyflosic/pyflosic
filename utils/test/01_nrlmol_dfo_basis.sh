9b7a587b04ae3b1658690ee8f0ee759446059a90
Jakob Kraus <kraus@theo.physik.tu-freiberg.de>
2020-06-18 14:46:38 +0200

    update

--- utils/test/01_nrlmol_dfo_basis.sh
+++
@@ -1,7 +0,0 @@
-# pyscf 
-export PYTHONPATH=/home/SHARE/for_sebastian/pyscf_sebastian/pyscf/:$PYTHONPATH
-# pyflosic 
-export PYTHONPATH=/home/SHARE/for_sebastian/pyflosic_master/src/:$PYTHONPATH
-export OMP_NUM_THREADS=24
-
-python ../nrlmol_basis.py
