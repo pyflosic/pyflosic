54a5e78d8b0186ede8fd03edfca19dcd7d1523b2
Jakob Kraus <kraus@theo.physik.tu-freiberg.de>
2020-06-29 13:56:54 +0200

    update

--- examples/ase_pyflosic_calculator/run_taurus.sh
+++
@@ -1,9 +0,0 @@
-module load  modenv/scs5 pyscf/1.6.0-intel-2018a-Python-3.6.4
-export PYTHONPATH=/home/sschwalb/__Programms__/ase/:$PYTHONPATH
-#export PYTHONPATH=/home/jakr872a/src/ase/:$PYTHONPATH
-export LD_PRELOAD=$MKLROOT/lib/intel64/libmkl_avx2.so:$MKLROOT/lib/intel64/libmkl_sequential.so:$MKLROOT/lib/intel64/libmkl_core.so
-# pyflosic 
-export PYTHONPATH=/projects/p_magmof/pyflosic_clean/src/:$PYTHONPATH
-export PYTHONPATH=/projects/p_magmof/pyflosic_clean/utils/:$PYTHONPATH
-
-python3 -W ignore $1
