2908a81f9ad47b810c23bc7e9048068b544e453b
theonov13 <theonov13@gmail.com>
2020-08-25 23:08:04 +0200

    add more packages and doc in init*sh

--- init_venv.sh
+++ init_venv.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-export PYTHONPATH=
 pip3 install virtualenv
 virtualenv -p /usr/bin/python3.7 venv
 source venv/bin/activate
 pip3 install ase==3.17.0
-pip3 install pyscf==1.7.1
+pip3 install h5py==2.10.0
 pip3 install numba==0.48.0
+pip3 install scipy==1.5.2
+pip3 install numpy==1.19.1
+pip3 install pyscf==1.7.1
+echo 'To deactivate the virtualenv environment simple type "deactivate" in our bash.'
