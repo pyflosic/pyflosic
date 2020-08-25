3665f297e7415b3f9630d30e252132e872e170ec
theonov13 <theonov13@gmail.com>
2020-08-25 21:15:09 +0200

    add more run.sh files

---
+++ examples/advanced_applications/run.sh
@@ -0,0 +1,18 @@
+# Run script for Pyflosic examples by LF.
+# Type in the example file you want to run.
+
+# Specify your PySCF path here.
+
+#export PYTHONPATH=/home/SHARE/for_sebastian/pyscf_sebastian/pyscf/:$PYTHONPATH
+#export PYTHONPATH=/home/schwalbe/__Programms__/pyscf_v3/pyscf/:$PYTHONPATH
+
+
+# Pyflosic path.
+cd ..
+pyflosic="$(dirname "$(pwd)")"
+cd advanced_applications
+export PYTHONPATH=$pyflosic/src/:$PYTHONPATH
+export PYTHONPATH=$pyflosic/utils/:$PYTHONPATH
+
+python3 $1
+
--- init_venv.sh
+++ init_venv.sh
@@ -1,5 +1,6 @@
+pip3 install virtualenv
 virtualenv -p /usr/bin/python3.7 venv
 source venv/bin/activate
-pip install ase==3.17.0
-pip install pyscf==1.7.1
-
+pip3 install ase==3.17.0
+pip3 install pyscf==1.6.1
+pip3 install numba==0.48.0
